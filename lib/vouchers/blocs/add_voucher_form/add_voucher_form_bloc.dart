01548a1b57271c86b85683c3933c3142311c4a83
Jure <jure.04jj@gmail.com>
2023-01-03 10:23:27 +0100

    cleanup

--- lib/vouchers/blocs/add_voucher_form/add_voucher_form_bloc.dart
+++ lib/vouchers/blocs/add_voucher_form/add_voucher_form_bloc.dart
@@ -10,8 +10,8 @@ import 'package:ms_kopalisce_admin_panel/vouchers/vouchers.dart';
 part 'add_voucher_form_event.dart';
 part 'add_voucher_form_state.dart';
 
-class AddVoucherFormBloc extends Bloc<AddVoucherFormEvent, AddVoucherFormState> {
-  AddVoucherFormBloc({
+abstract class _AddVoucherFormBloc extends Bloc<AddVoucherFormEvent, AddVoucherFormState> {
+  _AddVoucherFormBloc({
     Voucher? voucher,
     required CouponRepository couponRepository,
   })  : _couponRepository = couponRepository,
@@ -32,7 +32,7 @@ class AddVoucherFormBloc extends Bloc<AddVoucherFormEvent, AddVoucherFormState> 
     } else if (event is AddVoucherFormVoucherNumberUpdated) {
       yield _mapVoucherNumberUpdatedToState(event);
     } else if (event is AddVoucherFormSubmitted) {
-      yield* _mapFormSubmittedToState();
+      yield* mapFormSubmittedToState();
     }
   }
 
@@ -111,7 +111,14 @@ class AddVoucherFormBloc extends Bloc<AddVoucherFormEvent, AddVoucherFormState> 
     );
   }
 
-  Stream<AddVoucherFormState> _mapFormSubmittedToState() async* {
+  Stream<AddVoucherFormState> mapFormSubmittedToState();
+}
+
+class AddVoucherFormBloc extends _AddVoucherFormBloc {
+  AddVoucherFormBloc({required super.couponRepository});
+
+  @override
+  Stream<AddVoucherFormState> mapFormSubmittedToState() async* {
     final number = VoucherNumberFormz.dirty(state.number.value);
     final description = VoucherDescriptionFormz.dirty(state.description.value);
     final discountAmount = VoucherDiscountAmountFormz.dirty(state.discountAmount.value);
@@ -145,6 +152,60 @@ class AddVoucherFormBloc extends Bloc<AddVoucherFormEvent, AddVoucherFormState> 
         log('DiscountAMount: $discountAmount');
 
         DocumentReference reference = await _couponRepository.addVoucher(voucher);
+
+        voucher = voucher.copyWith(id: reference.id);
+
+        yield state.copyWith(
+          status: FormzStatus.submissionSuccess,
+          voucher: voucher,
+        );
+      } catch (e) {
+        yield state.copyWith(
+          status: FormzStatus.submissionFailure,
+        );
+      }
+    }
+  }
+}
+
+class AddVoucherWheelFormBloc extends _AddVoucherFormBloc {
+  AddVoucherWheelFormBloc({required super.couponRepository});
+
+  @override
+  Stream<AddVoucherFormState> mapFormSubmittedToState() async* {
+    final number = VoucherNumberFormz.dirty(state.number.value);
+    final description = VoucherDescriptionFormz.dirty(state.description.value);
+    final discountAmount = VoucherDiscountAmountFormz.dirty(state.discountAmount.value);
+    final name = VoucherNameFormz.dirty(state.name.value);
+    final unit = VoucherUnitFormz.dirty(state.unit.value);
+
+    final status = Formz.validate([number, description, discountAmount, name, unit]);
+
+    yield state.copyWith(
+      status: status,
+      description: description,
+      discountAmount: discountAmount,
+      name: name,
+      number: number,
+      unit: unit,
+    );
+
+    if (state.status.isValidated) {
+      try {
+        yield state.copyWith(status: FormzStatus.submissionInProgress);
+
+        Voucher voucher = Voucher(
+          description: state.description.value,
+          discountAmount: state.discountAmount.toInt(),
+          id: '',
+          name: state.name.value,
+          unit: state.unit.value,
+          voucherNumber: state.number.value,
+        );
+
+        log('DiscountAMount: $discountAmount');
+
+        DocumentReference reference = await _couponRepository.addVoucherToWheel(voucher);
 
         voucher = voucher.copyWith(id: reference.id);
 
--- lib/vouchers/blocs/blocs.dart
+++ lib/vouchers/blocs/blocs.dart
@@ -1,4 +1,3 @@
 export 'add_voucher_form/add_voucher_form_bloc.dart';
 export 'vouchers_list/vouchers_list_bloc.dart';
 export 'vouchers_list_wheel/vouchers_list_wheel_bloc.dart';
-export 'add_voucher_wheel_form/add_voucher_wheel_form_bloc.dart';
