ec4922ced1d9f3483a66a0a7d1202eae871f7f3e
Jure <jure.04jj@gmail.com>
2023-01-03 01:01:29 +0100

    cleanup

--- lib/prices/view/bathroom_services_view.dart
+++ lib/prices/view/bathroom_services_view.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ms_kopalisce_admin_panel/prices/blocs/blocs.dart';
+import 'package:price_repository/price_repository.dart';
 
 class BathroomTicketsView extends StatelessWidget {
   const BathroomTicketsView({Key? key}) : super(key: key);
 
+  static MaterialPageRoute route(BuildContext context) {
+    return MaterialPageRoute(builder: (context) {
+      return const BathroomTicketsView();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: BlocBuilder<BathroomServicesBloc, BathroomServicesState>(
+        builder: (context, state) {
+          if (state is BathroomServicesLoading) {
+            return const _LoadingScreen();
+          } else if (state is BathroomServicesLoadFail) {
+            return const _ErrorScreen();
+          } else if (state is BathroomServicesLoadSuccess) {
+            return BathroomServicesBuilder(tickets: state.tickets);
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+}
+
+class _LoadingScreen extends StatelessWidget {
+  const _LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
+
+class _ErrorScreen extends StatelessWidget {
+  const _ErrorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Sorry There was an error'),
+    );
   }
 }
---
+++ lib/prices/view/view.dart
@@ -0,0 +1,2 @@
+export 'add_bathroom_ticket_view.dart';
+export 'bathroom_services_view.dart';
