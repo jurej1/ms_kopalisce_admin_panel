e0281d1f26f8476d098a0d086fc4a88e9f0148f6
Jure <jure.04jj@gmail.com>
2023-01-05 09:30:21 +0100

    loading button

--- lib/quiz/widgets/add_question_submit_button.dart
+++ lib/quiz/widgets/add_question_submit_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
 
 import '../quiz.dart';
 
@@ -10,9 +11,10 @@ class AddQuestionSubmitButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<AddQuestionFormBloc, AddQuestionFormState>(
       builder: (context, state) {
+        final bool isLoading = state.status.isSubmissionInProgress;
         return ElevatedButton(
           key: UniqueKey(),
-          child: const Text('Submit'),
+          child: isLoading ? const CircularProgressIndicator() : const Text('Submit'),
           onPressed: () {
             BlocProvider.of<AddQuestionFormBloc>(context).add(AddQuestionFormSubmitted());
           },
